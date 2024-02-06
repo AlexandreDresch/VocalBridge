3da6ee05472b8e3653ca54c09a241fb79a00ae9e
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-06 00:13:15 -0300

    feat: add NewCard component

---
+++ src/components/new-card.tsx
@@ -0,0 +1,14 @@
+export default function NewCard() {
+    return (
+      <div className="rounded-sm bg-slate-700 p-5 space-y-3">
+        <span className="text-sm font-medium text-slate-200">Start the fun</span>
+  
+        <p className="text-sm leading-6 text-slate-400">
+          Record your voice and we will convert it into text in whatever language
+          you want.
+        </p>
+  
+      </div>
+    );
+  }
+  
