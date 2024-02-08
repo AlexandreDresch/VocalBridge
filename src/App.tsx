83989edfcae97c34a45da26d22653e0a5763008a
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-07 22:48:50 -0300

    feat: add cards dynamically to home based on notes from context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import Card from "./components/card";
 import Header from "./components/header";
 import NewCard from "./components/new-card";
+import { NotesContext } from "./providers/notes-context";
 
 export default function App() {
+  const { notes } = useContext(NotesContext);
+
   return (
     <div className="mx-auto my-12 max-w-6xl space-y-5">
       <Header />
@@ -10,10 +14,9 @@ export default function App() {
       <main className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewCard />
 
-        <Card
-          date={new Date()}
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, rerum officiis necessitatibus asperiores maiores quis? Architecto, nam impedit? Rem facere ipsum corrupti cupiditate perferendis commodi officia fugiat natus adipisci possimus?"
-        />
+        {notes.map((note) => (
+          <Card key={note.id} note={note} />
+        ))}
       </main>
     </div>
   );
