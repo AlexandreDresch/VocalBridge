10005aff08a7276897539e8cdace62a497fb7778
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-07 22:49:21 -0300

    feat: add search filter to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,16 @@
+import { ChangeEvent, useContext } from "react";
 import logo from "../assets/bridge.svg";
+import { NotesContext } from "../providers/notes-context";
 
-export default function Header() {
+export default function Header() {  
+  const { handleQuery } = useContext(NotesContext);
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+    const query = e.target.value;
+
+    handleQuery(query);
+  }
+
   return (
     <header className="w-full space-y-5">
       <div className="flex  items-center">
@@ -14,6 +24,7 @@ export default function Header() {
         <input
           type="text"
           placeholder="Search in your notes..."
+          onChange={handleSearch}
           className="w-full bg-transparent text-3xl font-medium tracking-tight placeholder:text-slate-500 outline-none"
         />
       </form>
