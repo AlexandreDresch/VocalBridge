928ad2dfdb6404e06dae9eaeeae05441a7980123
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-07 22:46:18 -0300

    feat: add context provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "sonner";
+import NotesProvider from "./providers/notes-context.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NotesProvider>
+      <App />
+    </NotesProvider>
     <Toaster richColors />
   </React.StrictMode>
 );
