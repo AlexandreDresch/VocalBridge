d3d69976379c2cd8e51c40f3bcb837e85f0a62d7
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-08 23:57:25 -0300

    feat: add responsiveness to grid

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ export default function App() {
   const { notes } = useContext(NotesContext);
 
   return (
-    <div className="mx-auto my-12 max-w-6xl space-y-5">
+    <div className="mx-auto my-12 max-w-6xl space-y-5 px-5">
       <Header />
 
-      <main className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewCard />
 
         {notes.map((note) => (
