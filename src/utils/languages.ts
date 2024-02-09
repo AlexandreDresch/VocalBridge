134006669e7dfb966d9a05c95c54174d58b2ceee
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-08 23:58:22 -0300

    feat: add languages array

---
+++ src/utils/languages.ts
@@ -0,0 +1,119 @@
+export const languages = [
+  {
+    name: "Arabic",
+    nativeName: "العربية",
+    slug: "ar",
+  },
+  {
+    name: "Bulgarian",
+    nativeName: "Български",
+    slug: "bg",
+  },
+  {
+    name: "German",
+    nativeName: "Deutsch",
+    slug: "de",
+  },
+  {
+    name: "Greek",
+    nativeName: "Ελληνικά",
+    slug: "el",
+  },
+  {
+    name: "English",
+    nativeName: "English",
+    slug: "en",
+  },
+  {
+    name: "Spanish",
+    nativeName: "Español",
+    slug: "es",
+  },
+  {
+    name: "French",
+    nativeName: "Français",
+    slug: "fr",
+  },
+  {
+    name: "Hebrew",
+    nativeName: "עברית",
+    slug: "he",
+  },
+  {
+    name: "Hindi",
+    nativeName: "हिन्दी",
+    slug: "hi",
+  },
+  {
+    name: "Italian",
+    nativeName: "Italiano",
+    slug: "it",
+  },
+  {
+    name: "Japanese",
+    nativeName: "日本語",
+    slug: "ja",
+  },
+  {
+    name: "Korean",
+    nativeName: "한국어",
+    slug: "ko",
+  },
+
+  {
+    name: "Dutch",
+    nativeName: "Nederlands",
+    slug: "nl",
+  },
+  {
+    name: "Polish",
+    nativeName: "Polski",
+    slug: "pl",
+  },
+  {
+    name: "Portuguese (Brazil)",
+    nativeName: "Português (Brasil)",
+    slug: "pt",
+  },
+  {
+    name: "Portuguese (Portugal)",
+    nativeName: "Português (Portugal)",
+    slug: "pt-PT",
+  },
+  {
+    name: "Romanian",
+    nativeName: "Română",
+    slug: "ro",
+  },
+  {
+    name: "Russian",
+    nativeName: "Русский",
+    slug: "ru",
+  },
+  {
+    name: "Swedish",
+    nativeName: "Svenska",
+    slug: "sv",
+  },
+
+  {
+    name: "Ukrainian",
+    nativeName: "Українська",
+    slug: "uk",
+  },
+  {
+    name: "Vietnamese",
+    nativeName: "Tiếng Việt",
+    slug: "vi",
+  },
+  {
+    name: "Chinese Traditional",
+    nativeName: "繁體中文 (繁體)",
+    slug: "zh-Hant",
+  },
+  {
+    name: "Zulu",
+    nativeName: "Isi-Zulu",
+    slug: "zu",
+  },
+];
