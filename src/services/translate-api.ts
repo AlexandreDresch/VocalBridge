5bd8e18c3bbf0d9716d21a780e338da9dc143bc5
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-15 00:01:03 -0300

    feat: change fetchTranslation result data

--- src/services/translate-api.ts
+++ src/services/translate-api.ts
@@ -1,32 +1,45 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from "axios";
 
-export async function fetchTranslation(text: string, language:string ) {
-  const options = {
-    method: 'POST',
-    url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+export interface ITranslationResult {
+  from: string;
+  to: string;
+  translation: string;
+}
+
+export async function fetchTranslation(text: string, language: string) : Promise<ITranslationResult> {
+  const options: AxiosRequestConfig = {
+    method: "POST",
+    url: "https://microsoft-translator-text.p.rapidapi.com/translate",
     params: {
-      'to[0]': language,
-      'api-version': '3.0',
-      profanityAction: 'NoAction',
-      textType: 'plain'
+      "to": language,
+      "api-version": "3.0",
+      profanityAction: "NoAction",
+      textType: "plain",
     },
     headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
-      'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+      "content-type": "application/json",
+      "X-RapidAPI-Key": import.meta.env.VITE_API_KEY as string,
+      "X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com",
     },
     data: [
       {
         Text: text,
-      }
-    ]
+      },
+    ],
   };
 
   try {
     const response = await axios.request(options);
-    return response.data[0].translations[0].text;
+
+    const result: ITranslationResult = {
+      from: response.data[0].detectedLanguage.language,
+      to: response.data[0].translations[0].to,
+      translation: response.data[0].translations[0].text,
+    };
+
+    return result;
   } catch (error) {
     console.error(error);
-    throw error; 
+    throw error;
   }
 }
