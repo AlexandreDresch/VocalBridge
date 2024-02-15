e8df6fa38dbed6fbd8680615a8526dfc1fe73193
AlexandreDresch <connectWithAlexandre@proton.me>
2024-02-14 23:59:30 -0300

    feat: add flags to languages array

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -3,117 +3,140 @@ export const languages = [
     name: "Arabic",
     nativeName: "العربية",
     slug: "ar",
+    flag: "🇦🇪"
   },
   {
     name: "Bulgarian",
     nativeName: "Български",
     slug: "bg",
+    flag: "🇧🇬"
   },
   {
     name: "German",
     nativeName: "Deutsch",
     slug: "de",
+    flag: "🇩🇪"
   },
   {
     name: "Greek",
     nativeName: "Ελληνικά",
     slug: "el",
+    flag: "🇬🇷"
   },
   {
     name: "English",
     nativeName: "English",
     slug: "en",
+    flag: "🇺🇸"
   },
   {
     name: "Spanish",
     nativeName: "Español",
     slug: "es",
+    flag: "🇪🇸"
   },
   {
     name: "French",
     nativeName: "Français",
     slug: "fr",
+    flag: "🇫🇷"
   },
   {
     name: "Hebrew",
     nativeName: "עברית",
     slug: "he",
+    flag: "🇮🇱"
   },
   {
     name: "Hindi",
     nativeName: "हिन्दी",
     slug: "hi",
+    flag: "🇮🇳"
   },
   {
     name: "Italian",
     nativeName: "Italiano",
     slug: "it",
+    flag: "🇮🇹"
   },
   {
     name: "Japanese",
     nativeName: "日本語",
     slug: "ja",
+    flag: "🇯🇵"
   },
   {
     name: "Korean",
     nativeName: "한국어",
     slug: "ko",
+    flag: "🇰🇷"
   },
 
   {
     name: "Dutch",
     nativeName: "Nederlands",
     slug: "nl",
+    flag: "🇳🇱"
   },
   {
     name: "Polish",
     nativeName: "Polski",
     slug: "pl",
+    flag: "🇵🇱"
   },
   {
-    name: "Portuguese (Brazil)",
+    name: "Portuguese",
     nativeName: "Português (Brasil)",
     slug: "pt",
+    flag: "🇧🇷"
   },
   {
-    name: "Portuguese (Portugal)",
+    name: "Portuguese",
     nativeName: "Português (Portugal)",
     slug: "pt-PT",
+    flag: "🇵🇹"
   },
   {
     name: "Romanian",
     nativeName: "Română",
     slug: "ro",
+    flag: "🇷🇴"
   },
   {
     name: "Russian",
     nativeName: "Русский",
     slug: "ru",
+    flag: "🇷🇺"
   },
   {
     name: "Swedish",
     nativeName: "Svenska",
     slug: "sv",
+    flag: "🇸🇪"
   },
 
   {
     name: "Ukrainian",
     nativeName: "Українська",
     slug: "uk",
+    flag: "🇺🇦"
   },
   {
     name: "Vietnamese",
     nativeName: "Tiếng Việt",
     slug: "vi",
+    flag: "🇻🇳"
   },
   {
     name: "Chinese Traditional",
     nativeName: "繁體中文 (繁體)",
     slug: "zh-Hant",
+    flag: "🇨🇳"
   },
   {
     name: "Zulu",
     nativeName: "Isi-Zulu",
     slug: "zu",
+    flag: "🇿🇦"
   },
 ];
